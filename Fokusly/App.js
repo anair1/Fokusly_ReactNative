020c01090a0dd556d2af909e74cb4b471a6fe24a
Arjun Nair <arjunnair.gm@gmail.com>
2023-11-11 01:50:18 -0500

    Setup the Initial MVP application
    - Can be ran on iOS 17
    - Opens Fokusly.com in a WebView container
    - Still need to get Apple Dev account to build application to run outside of Expo Go

--- Fokusly/App.js
+++ Fokusly/App.js
@@ -1,19 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+    style={styles.container}
+    source={{ uri: 'https://www.fokusly.com' }}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#076',
     alignItems: 'center',
     justifyContent: 'center',
   },
