4c327504a0ff66b139dd4abbef7fdced40c3747b
Arjun Nair <arjunnair.gm@gmail.com>
2023-11-16 23:33:32 -0500

    Creating basic sign on page

--- Fokusly/App.js
+++ Fokusly/App.js
@@ -1,20 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import * as React from 'react';
 import { WebView } from 'react-native-webview';
+import SignInButton from './SignInButton';
+import SignInLogic from './SignInLogic';
 
 export default function App() {
   return (
-    <WebView
-    style={styles.container}
-    source={{ uri: 'https://www.fokusly.com' }}
-    />
+    <View style={styles.container}>
+      <Text
+        style={{
+          marginBottom: 10
+        }}
+      >fokusly</Text>
+      <Text
+        style={{
+          marginBottom: 30
+        }}
+      >Bringing Focus to Social Media</Text>
+      <SignInLogic/>
+      <SignInButton/>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#076',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ Fokusly/SignInButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Button} from 'react-native';
+
+const SignInButton = () => {
+    return <Button title='Sign in'/>
+};
+
+export default SignInButton;
